e9f8e59a9d08e4934580aaa35b047b050e0ba05b
周珩 <zhouheng@yfkjqh.com>
2022-08-25 09:16:46 +0800

    删除无用代码

--- src/config/voice.js
+++ src/config/voice.js
@@ -3,12 +3,12 @@
  *
  * 实时转写调用demo
  * 此demo只是一个简单的调用示例，不适合用到实际生产环境中
- *  
+ *
  * 实时语音转写 WebAPI 接口调用示例 接口文档（必看）：https://www.xfyun.cn/doc/asr/rtasr/API.html
  * 错误码链接：
  * https://www.xfyun.cn/doc/asr/rtasr/API.html
  * https://www.xfyun.cn/document/error-code （code返回错误码时必看）
- * 
+ *
  */
 import CryptoJS from "crypto-js";
 import md5 from "md5";
@@ -16,175 +16,181 @@ import md5 from "md5";
 import TransWorker from "./transformpcm.worker.js";
 let recorderWorker = new TransWorker();
 // 记录处理的缓存音频
-let buffer = []
-let AudioContext = window.AudioContext || window.webkitAudioContext
-let notSupportTip = '请试用chrome浏览器且域名为localhost或127.0.0.1测试'
-navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia
+let buffer = [];
+let AudioContext = window.AudioContext || window.webkitAudioContext;
+let notSupportTip = "请试用chrome浏览器且域名为localhost或127.0.0.1测试";
+navigator.getUserMedia =
+  navigator.getUserMedia ||
+  navigator.webkitGetUserMedia ||
+  navigator.mozGetUserMedia ||
+  navigator.msGetUserMedia;
 
 recorderWorker.onmessage = function (e) {
-  buffer.push(...e.data.buffer)
-}
+  buffer.push(...e.data.buffer);
+};
 
 class IatRecorder {
   constructor(config) {
-    this.config = config
-    this.state = 'ing'
+    this.config = config;
+    this.state = "ing";
     //以下信息在控制台-我的应用-实时语音转写 页面获取
-    this.appId = '9e178222'
-    this.apiKey = '195bce25f2e2bdfd44b62fdcef0f11ec'
+    this.appId = "9e178222";
+    this.apiKey = "195bce25f2e2bdfd44b62fdcef0f11ec";
   }
 
   start() {
-    this.stop()
+    this.stop();
     if (navigator.getUserMedia && AudioContext) {
-      this.state = 'ing'
+      this.state = "ing";
       if (!this.recorder) {
-        var context = new AudioContext()
-        this.context = context
-        this.recorder = context.createScriptProcessor(0, 1, 1)
+        var context = new AudioContext();
+        this.context = context;
+        this.recorder = context.createScriptProcessor(0, 1, 1);
         var getMediaSuccess = (stream) => {
-          var mediaStream = this.context.createMediaStreamSource(stream)
-          this.mediaStream = mediaStream
+          var mediaStream = this.context.createMediaStreamSource(stream);
+          this.mediaStream = mediaStream;
           this.recorder.onaudioprocess = (e) => {
-            this.sendData(e.inputBuffer.getChannelData(0))
-          }
-          this.connectWebsocket()
-        }
+            this.sendData(e.inputBuffer.getChannelData(0));
+          };
+          this.connectWebsocket();
+        };
         var getMediaFail = (e) => {
-          this.recorder = null
-          this.mediaStream = null
-          this.context = null
-          console.log('请求麦克风失败'+e)
-        }
+          this.recorder = null;
+          this.mediaStream = null;
+          this.context = null;
+          console.log("请求麦克风失败" + e);
+        };
         if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
-          navigator.mediaDevices.getUserMedia({
-            audio: true,
-            video: false
-          }).then((stream) => {
-            getMediaSuccess(stream)
-          }).catch((e) => {
-            getMediaFail(e)
-          })
+          navigator.mediaDevices
+            .getUserMedia({
+              audio: true,
+              video: false,
+            })
+            .then((stream) => {
+              getMediaSuccess(stream);
+            })
+            .catch((e) => {
+              getMediaFail(e);
+            });
         } else {
-          navigator.getUserMedia({
-            audio: true,
-            video: false
-          }, (stream) => {
-            getMediaSuccess(stream)
-          }, function (e) {
-            getMediaFail(e)
-          })
+          navigator.getUserMedia(
+            {
+              audio: true,
+              video: false,
+            },
+            (stream) => {
+              getMediaSuccess(stream);
+            },
+            function (e) {
+              getMediaFail(e);
+            }
+          );
         }
       } else {
-        this.connectWebsocket()
+        this.connectWebsocket();
       }
     } else {
       // eslint-disable-next-line no-unused-vars
-      var isChrome = navigator.userAgent.toLowerCase().match(/chrome/)
-      alert(notSupportTip)
+      var isChrome = navigator.userAgent.toLowerCase().match(/chrome/);
+      alert(notSupportTip);
     }
   }
 
   stop() {
-    this.state = 'end'
+    this.state = "end";
     try {
-      this.mediaStream.disconnect(this.recorder)
-      this.recorder.disconnect()
+      this.mediaStream.disconnect(this.recorder);
+      this.recorder.disconnect();
     } catch (e) {
-      console.log(e)
+      console.log(e);
     }
   }
 
   sendData(buffer) {
     recorderWorker.postMessage({
-      command: 'transform',
-      buffer: buffer
-    })
+      command: "transform",
+      buffer: buffer,
+    });
   }
   // 生成握手参数
   getHandShakeParams() {
-    var appId = this.appId
-    var secretKey = this.apiKey
-    var ts = Math.floor(new Date().getTime() / 1000);//new Date().getTime()/1000+'';
-    var signa = md5(appId + ts)//hex_md5(encodeURIComponent(appId + ts));//EncryptUtil.HmacSHA1Encrypt(EncryptUtil.MD5(appId + ts), secretKey);
-    var signatureSha = CryptoJS.HmacSHA1(signa, secretKey)
-    var signature = CryptoJS.enc.Base64.stringify(signatureSha)
-    signature = encodeURIComponent(signature)
+    var appId = this.appId;
+    var secretKey = this.apiKey;
+    var ts = Math.floor(new Date().getTime() / 1000);
+    var signa = md5(appId + ts);
+    var signatureSha = CryptoJS.HmacSHA1(signa, secretKey);
+    var signature = CryptoJS.enc.Base64.stringify(signatureSha);
+    signature = encodeURIComponent(signature);
     return "?appid=" + appId + "&ts=" + ts + "&signa=" + signature;
   }
 
   connectWebsocket() {
-    var url = 'wss://rtasr.xfyun.cn/v1/ws'
-    var urlParam = this.getHandShakeParams()
-    url = `${url}${urlParam}`
-    if ('WebSocket' in window) {
-      this.ws = new WebSocket(url)
-    } else if ('MozWebSocket' in window) {
+    var url = "wss://rtasr.xfyun.cn/v1/ws";
+    var urlParam = this.getHandShakeParams();
+    url = `${url}${urlParam}`;
+    if ("WebSocket" in window) {
+      this.ws = new WebSocket(url);
+    } else if ("MozWebSocket" in window) {
       // eslint-disable-next-line no-undef
-      this.ws = new MozWebSocket(url)
+      this.ws = new MozWebSocket(url);
     } else {
-      alert(notSupportTip)
-      return null
+      alert(notSupportTip);
+      return null;
     }
     this.ws.onopen = (e) => {
-      this.mediaStream.connect(this.recorder)
-      this.recorder.connect(this.context.destination)
+      this.mediaStream.connect(this.recorder);
+      this.recorder.connect(this.context.destination);
       setTimeout(() => {
-        this.wsOpened(e)
-      }, 500)
-    }
+        this.wsOpened(e);
+      }, 500);
+    };
     this.ws.onmessage = (e) => {
-      // this.config.onMessage && this.config.onMessage(e)
-      this.wsOnMessage(e)
-    }
+      this.wsOnMessage(e);
+    };
     this.ws.onerror = (e) => {
-      this.stop()
-      console.log("关闭连接ws.onerror"+e);
-    }
+      this.stop();
+      console.log("关闭连接ws.onerror" + e);
+    };
     this.ws.onclose = (e) => {
-      this.stop()
-      console.log("关闭连接ws.onclose"+e);
-    }
+      this.stop();
+      console.log("关闭连接ws.onclose" + e);
+    };
   }
 
   wsOpened() {
     if (this.ws.readyState !== 1) {
-      return
+      return;
     }
-    var audioData = buffer.splice(0, 1280)
-    this.ws.send(new Int8Array(audioData))
+    var audioData = buffer.splice(0, 1280);
+    this.ws.send(new Int8Array(audioData));
     this.handlerInterval = setInterval(() => {
       // websocket未连接
       if (this.ws.readyState !== 1) {
-        clearInterval(this.handlerInterval)
-        return
+        clearInterval(this.handlerInterval);
+        return;
       }
       if (buffer.length === 0) {
-        if (this.state === 'end') {
-          this.ws.send("{\"end\": true}")
+        if (this.state === "end") {
+          this.ws.send('{"end": true}');
           console.log("发送结束标识");
-          clearInterval(this.handlerInterval)
+          clearInterval(this.handlerInterval);
         }
-        return false
+        return false;
       }
-      var audioData = buffer.splice(0, 1280)
+      var audioData = buffer.splice(0, 1280);
       if (audioData.length > 0) {
-        this.ws.send(new Int8Array(audioData))
+        this.ws.send(new Int8Array(audioData));
       }
-    }, 40)
+    }, 40);
   }
 
   wsOnMessage(e) {
-    let jsonData = JSON.parse(e.data)
+    let jsonData = JSON.parse(e.data);
     if (jsonData.action == "started") {
       // 握手成功
       console.log("握手成功");
     } else if (jsonData.action == "result") {
-      // 转写结果
-      // if (this.config.onMessage && typeof this.config.onMessage == 'function') {
-        this.result(jsonData.data);
-        // this.config.onMessage(jsonData.data)
-      // }
+      this.result(jsonData.data);
     } else if (jsonData.action == "error") {
       // 连接发生错误
       console.log("出错了:", jsonData);
@@ -192,23 +198,22 @@ class IatRecorder {
   }
 
   result(data) {
-    let rtasrResult = []
-    data=JSON.parse(data)
-    rtasrResult[data.seg_id] = data
-    rtasrResult.forEach(i => {
+    let rtasrResult = [];
+    data = JSON.parse(data);
+    rtasrResult[data.seg_id] = data;
+    rtasrResult.forEach((i) => {
       let str = "";
-     
-      i.cn.st.rt.forEach(j => {
-        j.ws.forEach(k => {
-          k.cw.forEach(l => {
-            str += l.w
-          })
-        })
-      })
-      
+
+      i.cn.st.rt.forEach((j) => {
+        j.ws.forEach((k) => {
+          k.cw.forEach((l) => {
+            str += l.w;
+          });
+        });
+      });
+
       this.onTextChange && this.onTextChange(str || "");
-    
-    })
+    });
   }
 }
 
